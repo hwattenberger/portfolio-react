9b4bd4dbccc412a49ac60ed30c4a481a36668448
Hilary Wattenberger <Hwattenberger@gmail.com>
2021-11-13 22:05:40 -0600

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import noriBox from './images/nori-box.png'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <main>
+        <div id="intro" className="h-screen flex flex-col justify-center	bg-purple-300	">
+          <p>Hi - I'm...</p>
+          <h1 className="text-4xl	font-bold	">Hilary Wattenberger</h1>
+          <p>Thanks for visiting!  Take a look at some of the projects I've been working on recently.</p>
+          <a href="#work"><button className="px-8 py-2 mt-4 bg-gray-400	rounded-md	">Learn More</button></a>
+          <img src={noriBox} className="object-none"></img>
+        </div>
+      </main>
     </div>
   );
 }
