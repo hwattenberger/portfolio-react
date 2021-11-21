ad784a4e42f692800321ed42f39219b2c3d5baa1
Hilary Wattenberger <Hwattenberger@gmail.com>
2021-11-20 22:30:10 -0600

    Add loading

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 
 import './App.css';
 import Projects from './components/Projects';
@@ -8,24 +8,39 @@ import noriBox from './images/nori-box.png'
 
 function App() {
   const [projectNum, setProjectNum] = useState(1);
+  const [showMain, setShowMain] = useState(false);
+
+  useEffect(()=> {
+    setTimeout(()=>setShowMain(true),7000)
+  }, []);
+
+  if(!showMain) {
+    return (
+      <div className="App h-screen bg-purple-300 flex items-center justify-center">
+        <img src={noriBox} className="object-none main-nori-img"></img>
+      </div>
+    )
+  }
 
   return (
     <div className="App">
       <header className="App-header">
       </header>
       <main>
-        <div id="intro" className="h-screen flex flex-col justify-center bg-purple-300">
+        <div id="intro" className="h-screen flex items-center flex-col justify-center bg-purple-300 text-center">
           <p>Hi - I'm...</p>
           <h1 className="text-4xl	font-bold	">Hilary Wattenberger</h1>
           <p>Thanks for visiting!  Take a look at some of the projects I've been working on recently.</p>
-          <a href="#work"><button className="px-8 py-2 mt-4 bg-gray-400	rounded-md	">Learn More</button></a>
-          <img src={noriBox} className="object-none"></img>
+          <div>
+            <a href="#work"><button className="px-8 py-2 mt-4 bg-gray-400	rounded-md m-2 transform hover:-translate-y-1 transition duration-500 ease-in-out">Projects</button></a>
+            <a href="#aboutMe"><button className="px-8 py-2 mt-4 bg-gray-400 rounded-md	transform hover:-translate-y-1 transition duration-500 ease-in-out">About Me</button></a>
+          </div>
         </div>
-        <div className="min-h-screen flex flex-col lg:flex-row">
+        <div id="work" className="min-h-screen flex flex-col lg:flex-row">
           <MainProject projectNum={projectNum} />
           <Projects projectNum={projectNum} setProjectNum={setProjectNum}/>
         </div>
-        <div className="flex flex-col items-center justify-center bg-purple-300">
+        <div id="aboutMe" className="flex flex-col items-center justify-center bg-purple-300">
           <div className="md:w-6/12 bg-purple-100 p-4 m-4 rounded">
             <h2 className="text-3xl p-2">About Me</h2>
             <p className="p-2"><span className="font-semibold">Origin Story:</span> My coding journey began back in middle school.  I found this amazing website called Neopets and created a guild and to create a webpage for that guild, I needed to know
